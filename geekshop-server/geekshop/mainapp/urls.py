a054e7e0d297d3b836652489f58e14eb0e4be7b4
Danila <kalganovdanila@gmail.com>
2021-12-23 21:52:17 +0300

    [upd] Устранил некоторые недоработки и сделал страницу для продукта.

--- geekshop-server/geekshop/mainapp/urls.py
+++ geekshop-server/geekshop/mainapp/urls.py
@@ -7,5 +7,6 @@ app_name = 'mainapp'
 urlpatterns = [
    path('', mainapp.index, name='index'),
    path('products/category/<int:pk>', mainapp.products, name='products'),
+   path('products/product/<int:pk>', mainapp.product, name='product'),
    path('contact/', mainapp.contact, name='contact'),
 ]
--- geekshop-server/geekshop/mainapp/views.py
+++ geekshop-server/geekshop/mainapp/views.py
@@ -4,39 +4,58 @@ from basketapp.models import Basket
 import os
 
 
+def get_product_category():
+    return ProductCategory.objects.all()
+
+
+def get_basket(request):
+    if request.user.is_authenticated:
+        return Basket.objects.filter(user=request.user)
+
+
 def index(request):
     products_in_main = Product.objects.all()
     content = {
         'title': 'Главная',
         'menu': menu,
+        'basket': get_basket(request),
         'products': products_in_main
     }
     return render(request, 'mainapp/index.html', content)
 
 
 def products(request, pk):
-    basket = []
-    if request.user.is_authenticated:
-        basket = Basket.objects.filter(user=request.user)
-    products_menu = ProductCategory.objects.all()
     if pk == 0:
-        products = Product.objects.all()
+        all_products = Product.objects.all()
     else:
-        products = Product.objects.filter(category__pk=pk)
+        all_products = Product.objects.filter(category__pk=pk)
     content = {
         'title': 'Продукты',
         'menu': menu,
-        'products_menu': products_menu,
-        'products': products,
-        'basket': basket,
+        'products_menu': get_product_category(),
+        'products': all_products,
+        'basket': get_basket(request),
     }
     return render(request, 'mainapp/products.html', content)
+
+
+def product(request, pk):
+    title = 'продукт'
+    content = {
+        'title': title,
+        'menu': menu,
+        'products_menu': get_product_category(),
+        'product': get_object_or_404(Product, pk=pk),
+        'basket': get_basket(request),
+    }
+    return render(request, 'mainapp/product.html', content)
 
 
 def contact(request):
     content = {
         'title': 'Контакты',
         'menu': menu,
+        'basket': get_basket(request),
     }
     return render(request, 'mainapp/contact.html', content)
 
